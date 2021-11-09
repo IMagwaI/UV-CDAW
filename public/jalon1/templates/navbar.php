49eeae32aa0ea4cdb48f4812e2469107a4e9468b
Atef <atifabdelkader@gmail.com>
2021-11-09 15:05:54 +0100

    Update navbar.php

--- public/jalon1/templates/navbar.php
+++ public/jalon1/templates/navbar.php
@@ -1,13 +1,23 @@
 <!-- Navigation-->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+<nav class="navbar navbar-expand-lg  fixed-top" id="mainNav" style="background-color: #950101">
     <div class="container px-4">
-        <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
+        <a class="navbar-brand" href="#page-top" style="color:white">
+        <img src="../assets/logo.png" alt=""> Movie time</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
-                <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+            <ul class="navbar-nav ms-auto" >
+                <li class="nav-item"><a class="nav-link" href="#about" style="color:white">Favorie</a></li>
+                <li class="nav-item"><a class="nav-link" href="#services" style="color:white">Playlist</a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:white">
+                        Compte
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Profil</a>
+                        <a class="dropdown-item" href="#">Historique</a>
+                        <a class="dropdown-item" href="#">Se déconnecter</a>
+                    </div>
+                </li>
             </ul>
         </div>
     </div>
