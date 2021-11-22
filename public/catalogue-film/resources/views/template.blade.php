d0d8d71c93ef18f41cd06bb59add4aa42cc350db
Abderrahim Tantaoui <abdoutanta@gmail.com>
2021-11-22 16:56:50 +0100

    navbar changes

--- public/catalogue-film/resources/views/template.blade.php
+++ public/catalogue-film/resources/views/template.blade.php
@@ -55,41 +55,44 @@
                         <li><a class="nav-link scrollto" href="{{ route('movie-details') }}">Film</a></li>
                         <li><a class="nav-link scrollto" href="{{ route('playlist') }}">Playlist</a></li>
                     @else
-                        <li><a class="nav-link scrollto" href="{{ route('addFilm') }}">CRUD (Jalon 2)</a></li>
+                        {{-- <li><a class="nav-link scrollto" href="{{ route('addFilm') }}">CRUD (Jalon 2)</a></li> --}}
+                        <li><a class="nav-link scrollto" href="#main">Discover</a></li>
+
                     @endauth
 
-                    <li class="dropdown"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z">
-                                </path>
-                            </svg><span> Compte</span> <i class="bi bi-chevron-down"></i></a>
-                        <ul>
-                            <li><a href="{{ route('profil') }}">Mon profil</a></li>
-                            <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i
-                                        class="bi bi-chevron-right"></i></a>
-                                <ul>
-                                    <li><a href="#">Deep Drop Down 1</a></li>
-                                    <li><a href="#">Deep Drop Down 2</a></li>
-                                    <li><a href="#">Deep Drop Down 3</a></li>
-                                    <li><a href="#">Deep Drop Down 4</a></li>
-                                    <li><a href="#">Deep Drop Down 5</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Drop Down 2</a></li>
-                            <li><a href="#">Drop Down 3</a></li>
-                            <li><a href="#">Drop Down 4</a></li>
-                        </ul>
-                    </li>
-                    </li>
+                    <!-- .navbar -->
+                    @auth
+                        <li class="dropdown"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                    height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z">
+                                    </path>
+                                </svg><span> Compte</span> <i class="bi bi-chevron-down"></i></a>
+                            <ul>
+                                <li><a href="{{ route('profil') }}">Mon profil</a></li>
+                                {{-- <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i
+                                            class="bi bi-chevron-right"></i></a>
+                                    <ul>
+                                        <li><a href="#">Deep Drop Down 1</a></li>
+                                        <li><a href="#">Deep Drop Down 2</a></li>
+                                        <li><a href="#">Deep Drop Down 3</a></li>
+                                        <li><a href="#">Deep Drop Down 4</a></li>
+                                        <li><a href="#">Deep Drop Down 5</a></li>
+                                    </ul>
+                                </li> --}}
+                                <li><a href="#">Historique</a></li>
+                                <li><a href="#">Favoris</a></li>
+                                <li><a href="#">Playlists</a></li>
+                                <li><a href="#" class="btn-danger text-white">Déconnexion</a></li>
+                            </ul>
+                        </li>
 
-                </ul>
-                <i class="bi bi-list mobile-nav-toggle"></i>
-            </nav>
-            <!-- .navbar -->
-
-            <a href="{{ route('login') }}" class="get-started-btn scrollto">Connexion</a>
-
-
+                    </ul>
+                    <i class="bi bi-list mobile-nav-toggle"></i>
+                </nav>
+            @else
+                </nav>
+                <a href="{{ route('login') }}" class="get-started-btn scrollto">Connexion</a>
+            @endauth
         </div>
     </header>
     <!-- End Header -->
